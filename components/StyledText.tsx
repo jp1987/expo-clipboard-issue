27486a1dafd0d86c799343a9fe2833845d4cc9b3
Jesper Paardekooper <j.paardekooper@rinkel.nl>
2024-02-02 09:53:29 +0100

    chore: add clipboard test

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,5 +1,18 @@
+import * as Clipboard from 'expo-clipboard';
 import { Text, TextProps } from './Themed';
+import { useState } from 'react';
 
 export function MonoText(props: TextProps) {
+  const [copiedText, setCopiedText] = useState('');
+
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync('hello world');
+  };
+
+  const fetchCopiedText = async () => {
+    const text = await Clipboard.getStringAsync();
+    setCopiedText(text);
+  };
+
   return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
 }
